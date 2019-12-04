cde4a79b71103d2ec56d2c9dd71a924a3ce23ef0
Your NameDamian <damian18@ru.is>
2019-12-04 10:48:05 +0000

    createPlane

--- Create/createPlane.py
+++ Create/createPlane.py
@@ -11,6 +11,12 @@ class Create(Logic, Data):
         planeInsignia_str = input("Input plane insignia: ")
 
         planeTypeId_str = input("Input plane type ID: ")
+        validPlaneId_list = ["NAFokkerF28", "NAFokkerF100", "NABAE146"]
+
+        #Validity check for planeTypeId
+        while planeTypeId_str not in validPlaneId_list:
+            print("Please input valid plane type ID")
+            planeTypeId_str = input("Input plane type ID: ")
 
         plane_str = (planeInsignia_str + "," + planeTypeId_str)
 
