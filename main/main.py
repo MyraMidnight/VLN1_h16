a1a28a218e569b8cb6e382912146aa9350c983bf
MyraMidnight <dark_stardragon@hotmail.com>
2019-12-04 09:27:19 +0000

    some code into main.py and recreated the layer class diagram

--- main/main.py
+++ main/main.py
@@ -1,2 +1,21 @@
 # Here we will create the main program
 # and import classes from the 'modules' subfolder
+
+# ------------------------  Import classes
+
+# ------------------------  Global variables
+ROLE_PILOT = "pilot"
+ROLE_CAPTAIN = "captain"
+ROLE_COPILOT = "coPilot"
+ROLE_ATTENDANT = "flightAttendant"
+ROLE_LEAD_ATTENDANT = "headFlightAttendant"
+MODEL_PLANE = "" #instance of plane
+MODEL_EMPLOYEE = "" #instance of employee
+
+# ------------------------  Main program
+
+def main():
+  """The core program"""
+
+# ------------------------  Run program
+main()
