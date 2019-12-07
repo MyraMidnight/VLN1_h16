312bb19f4b38def12a6939beb525d7291fe44522
MyraMidnight <dark_stardragon@hotmail.com>
2019-12-07 19:24:20 +0000

    DateUtil is nearly finished

--- main/modules/ui_layer/DateUtil.py
+++ main/modules/ui_layer/DateUtil.py
@@ -1,29 +1,47 @@
 class DateUtil:
-    """Our own date parser!"""
-    def __init__(self, datetime:str):
-        self.datetime: datetime
-        self.testString: "1111-11-11T00:00:00"
+    def __init__(self):
+        self.date = None
+        self.time = None
+        self.day = None
+        self.month = None
+        self.year = None
+        self.hour = None
+        self.minute = None
 
-    def date(self, format:str = "num"):
+    def parse(self, datetime:str):
+        self.date = self.date_parse(datetime)
+        self.time = self.time_parse(datetime)
+        self.day = self.day_parse(datetime)
+        self.month = self.month_parse(datetime)
+        self.year = self.year_parse(datetime)
+        self.hour = self.hour_parse(datetime)
+        self.minute = self.hour_parse(datetime)
+
+    def date_parse(self, datetime:str, format:str = "num"):
         """returns date"""
-        return self.testSTring
+        return datetime[0:10]
 
-    def day(self, format:str = "num"):
+    def day_parse(self, datetime:str, format:str = "num"):
         """returns the day"""
-        
-    def month(self, format:str = "num"):
+        return datetime[8:10]
+
+    def month_parse(self, datetime:str, format:str = "num"):
         """returns the month"""
-        
-    def year(self, format:str = "num"):
+        return datetime[5:7]
+
+    def year_parse(self, datetime:str, format:str = "num"):
         """returns the year"""
-        
-    def time(self, format:str = "num"):
+        return datetime[:4]
+
+    def time_parse(self, datetime:str, format:str = "num"):
         """returns the time"""
-        return self.datetime
-        
-    def hour(self, format:str = "num"):
+        return datetime[-8:]
+
+    def hour_parse(self, datetime:str, format:str = "num"):
         """returns the hour"""
-        
-    def minute(self, format:str = "num"):
+        return datetime[-5:-2]
+
+    def minute_parse(self, datetime:str, format:str = "num"):
         """returns the minute"""
+        return datetime[-2:]
 
--- main/test_stefania.py
+++ main/test_stefania.py
@@ -2,15 +2,14 @@
 #from modules.data_layer.IOAPI import IOAPI
 from modules.ui_layer.InputHandler import InputHandler
 from modules.ui_layer.DateUtil import DateUtil
-import datetime
 
-ROLE_PILOT = "pilot"
-ROLE_CAPTAIN = "captain"
-ROLE_COPILOT = "coPilot"
-ROLE_ATTENDANT = "flightAttendant"
-ROLE_LEAD_ATTENDANT = "headFlightAttendant"
-MODEL_PLANE = "" #instance of plane
-MODEL_EMPLOYEE = "" #instance of employee
+# ROLE_PILOT = "pilot"
+# ROLE_CAPTAIN = "captain"
+# ROLE_COPILOT = "coPilot"
+# ROLE_ATTENDANT = "flightAttendant"
+# ROLE_LEAD_ATTENDANT = "headFlightAttendant"
+# MODEL_PLANE = "" #instance of plane
+# MODEL_EMPLOYEE = "" #instance of employee
 
 #ata = IOAPI().opener("Crew.csv")
 # for line in data:
@@ -19,6 +18,12 @@ MODEL_EMPLOYEE = "" #instance of employee
 
 date = InputHandler().dateOnly()
 
-print(date)
-dateOnly = DateUtil(date)
-print(dateOnly.date())
+parsedDate = DateUtil()
+parsedDate.parse(date)
+
+print("day: ",parsedDate.day)
+print("month: ",parsedDate.month)
+print("year: ",parsedDate.year)
+print("time: ",parsedDate.time)
+print("hour: ",parsedDate.hour)
+print("minute: ",parsedDate.minute)
