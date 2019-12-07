4a646b10956cdb91164110a7669a53b5a01064f4
MyraMidnight <dark_stardragon@hotmail.com>
2019-12-07 10:06:47 +0000

    finished dateOnly(), dateTime(), timeOnly() for inputHandler

--- main/test_stefania.py
+++ main/test_stefania.py
@@ -1,5 +1,6 @@
-from modules.ui_layer.DisplayScreen import DisplayScreen
-from modules.data_layer.IOAPI import IOAPI
+#from modules.ui_layer.DisplayScreen import DisplayScreen
+#from modules.data_layer.IOAPI import IOAPI
+from modules.ui_layer.InputHandler import InputHandler
 
 ROLE_PILOT = "pilot"
 ROLE_CAPTAIN = "captain"
@@ -9,7 +10,10 @@ ROLE_LEAD_ATTENDANT = "headFlightAttendant"
 MODEL_PLANE = "" #instance of plane
 MODEL_EMPLOYEE = "" #instance of employee
 
-data = IOAPI().opener("Crew.csv")
+#ata = IOAPI().opener("Crew.csv")
 # for line in data:
 #   print(line)
-DisplayScreen().printListFormat(data,rowLimit=3)
+#DisplayScreen().printListFormat(data,rowLimit=3)
+
+date = InputHandler().name()
+print(date)
